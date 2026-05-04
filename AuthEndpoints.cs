c898e1dab8d247bf1005a25e768ef57added3ee6
KutayKerem <96310892+KutayKerem@users.noreply.github.com>
2026-05-04 11:40:57 +0300

    LRP Vize Ödevi: Tüm fonksiyonlar, SPA mimarisi ve zimmetleme sistemi tamamlandı.
    Auth sistemi, otomatik demirbaş kodu üretimi ve öğrenci portalı modülleri ödev gereksinimlerine göre entegre edildi.

---
+++ AuthEndpoints.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using LabYonetimSistemi.Models;
+namespace LabYonetimSistemi.Endpoints;
+
+public static class AuthEndpoints
+{
+    public static void MapAuthEndpoints(this IEndpointRouteBuilder routes)
+    {
+        routes.MapPost("/api/login", async (AppDbContext db, User loginData) => {
+            var user = await db.Users.FirstOrDefaultAsync(u =>
+                u.Username.ToLower() == loginData.Username.ToLower() &&
+                u.Password == loginData.Password);
+
+            if (user == null) return Results.Unauthorized();
+            return Results.Ok(new { username = user.Username, role = user.Role });
+        });
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using LabYonetimSistemi.Models;
+using LabYonetimSistemi.Endpoints; // Yeni klasörün
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// --- 1. SERVİS KAYITLARI ---
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// --- 2. ARA YAZILIMLAR ---
+app.UseDefaultFiles();
+app.UseStaticFiles();
+app.UseSwagger();
+app.UseSwaggerUI();
+
+// --- 3. MODÜLLERİN KAYDI (YENİ YAPI) ---
+// Bu metodlar Endpoints klasöründeki static sınıflardan geliyor.
+app.MapAuthEndpoints();
+app.MapStatEndpoints();
+app.MapLabEndpoints();
+
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+    // VERİTABANINI VE TABLOLARI OTOMATİK OLUŞTUR (YENİ EKLE)
+    context.Database.EnsureCreated();
+
+    // Eğer Users tablosu boşsa veya admin yoksa ekle
+    if (!context.Users.Any(u => u.Username == "admin"))
+    {
+        context.Users.Add(new User
+        {
+            Username = "admin",
+            Password = "1234",
+            Role = "Admin"
+        });
+        context.SaveChanges();
+    }
+}
+// --- SEEDING BİTTİ ---
+
+app.Run();
